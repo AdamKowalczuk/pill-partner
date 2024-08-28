27eb8dd8475d3fc657a7839e9263b011dff7a93a
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2024-08-28 18:43:22 +0200

    Add custom colors and global styles

---
+++ styles/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  error100: "#FECACA",
+  error500: "#E63535",
+  error900: "#991B1B",
+  green100: "#DCFCE7",
+  green500: "#22C55E",
+  green900: "#14532D",
+  primary100: "#808080",
+  primary900: "#0A0A0A",
+  border: "#D3D3D3",
+  typography600: "#737373",
+  typography900: "#262627",
+};
---
+++ styles/global.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  authLayout: {
+    paddingTop: 32,
+    paddingBottom: 32,
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+});
