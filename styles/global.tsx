95512344f11c0dd3ce1f4d4b7fa5bdaa3c8e0419
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2024-09-01 19:06:15 +0200

    Change styles and colors

--- styles/global.tsx
+++ styles/global.tsx
@@ -6,5 +6,36 @@ export const globalStyles = StyleSheet.create({
     paddingBottom: 32,
     paddingLeft: 24,
     paddingRight: 24,
+    display: "flex",
+    gap: 32,
+  },
+  authImage: {
+    width: "50%",
+  },
+  authTopWrapper: {
+    display: "flex",
+  },
+  authHeader: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  authTextWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: 6,
+    fontSize: 14,
+  },
+  authText: {
+    color: "#525252",
+  },
+  authButtonWrapper: {
+    display: "flex",
+    gap: 20,
+  },
+  rememberPasswordButton: {
+    display: "flex",
+    alignItems: "flex-end",
   },
 });
