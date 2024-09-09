768dcb52d689d1fe1c9cccc99c130e81c037a401
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2024-09-09 18:32:54 +0200

    Add new colors

--- styles/colors.ts
+++ styles/colors.ts
@@ -1,17 +1,30 @@
 export const colors = {
   error100: "#FECACA",
+  error200: "#FCA5A5",
   error500: "#E63535",
+  error700: "#B91C1C",
   error900: "#991B1B",
+
   green100: "#DCFCE7",
+  green200: "BBF7D0",
   green500: "#22C55E",
+  green700: "#15803D",
   green900: "#14532D",
-  // primary100: "#808080",
-  // primary900: "#0A0A0A",
+
+  yellow200: "#FEF08A",
+  yellow700: "#A16207",
+
+  lime200: "#D9F99D",
+  lime700: "#4D7C0F",
 
   white: "#fff",
   border300: "#D3D3D3",
+
+  typography500: "#8C8C8C",
   typography600: "#737373",
+  typography700: "#525252",
   typography900: "#262627",
+
   primary50: "#EEF2FF",
   primary100: "#E0E7FF",
   primary200: "#C7D2FE",
