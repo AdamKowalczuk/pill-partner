d215013e66fed1f8e0c9b49e1244108677ee3dc2
AdamKowalczuk <57939211+AdamKowalczuk@users.noreply.github.com>
2024-09-26 20:17:13 +0200

    add medication stock screen

--- src/screens/medication/MedicationItemStock.tsx
+++ src/screens/medication/MedicationItemStock.tsx
@@ -1,15 +1,147 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { IconPillFilled, IconDotsVertical } from "@tabler/icons-react";
+import { Menu, MenuItem, MenuItemLabel } from "@/src/components/ui/menu";
+import { Button, ButtonIcon, ButtonText } from "@/src/components/ui/button";
+import { IconTrash, IconEdit } from "@tabler/icons-react";
+import {
+  Modal,
+  ModalBackdrop,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@/src/components/ui/modal";
+import { Heading } from "@/src/components/ui/heading";
+import { CloseIcon, Icon } from "@/src/components/ui/icon";
 
 type MedicationItemStockProps = {
   name: string;
 };
 
-const MedicationItemStock = ({ name }: MedicationItemStockProps) => {
+const MedicationItemStock = ({ medication }: any) => {
+  console.log("medication", medication);
+  const [showModal, setShowModal] = React.useState(false);
+  const ref = React.useRef(null);
+
   return (
-    <View>
-      <Text>{name}</Text>
-    </View>
+    <>
+      <View className="flex flex-row gap-4 rounded p-4 border-solid border border-border-300">
+        <View>
+          <IconPillFilled
+            stroke={2}
+            className="w-[32px] h-[32px] fill-primary-500"
+          />
+        </View>
+
+        <View className="flex gap-5 flex-1">
+          <View className="flex flex-row justify-between">
+            <Text className="text-typography-900">
+              {medication?.medicationName}
+            </Text>
+            {/* <IconDotsVertical className="cursor-pointer" stroke={2} /> */}
+            <Menu
+              placement="bottom right"
+              selectionMode="single"
+              offset={5}
+              onSelectionChange={(keys) => {
+                if (keys.currentKey === "Edytuj") {
+                  console.log("Edytuj");
+                } else if (keys.currentKey === "Usuń") {
+                  console.log("Usuń");
+                  setShowModal(true);
+                }
+              }}
+              trigger={({ ...triggerProps }) => {
+                return (
+                  <Button {...triggerProps} className="bg-transparent">
+                    <ButtonIcon as={IconDotsVertical} />
+                  </Button>
+                );
+              }}
+            >
+              <MenuItem
+                key="Edytuj"
+                textValue="Edytuj"
+                className="p-2 min-w-[294px]"
+              >
+                <IconEdit stroke={1.33} />
+                <MenuItemLabel size="sm">Edytuj</MenuItemLabel>
+              </MenuItem>
+              <MenuItem
+                key="Usuń"
+                textValue="Usuń"
+                className="p-2 min-w-[294px]"
+              >
+                <IconTrash stroke={1.33} />
+                <MenuItemLabel size="sm">Usuń</MenuItemLabel>
+              </MenuItem>
+            </Menu>
+          </View>
+          <View className="flex gap-3">
+            <View className="flex flex-row justify-between">
+              <Text className="text-typography-500">Wystarczy na</Text>
+              <Text>2 dni</Text>
+            </View>
+            <View className="flex flex-row justify-between">
+              <Text className="text-typography-500">W zapasie</Text>
+              <Text>{medication?.stockAmount} tabletki</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <Modal
+        isOpen={showModal}
+        onClose={() => {
+          setShowModal(false);
+        }}
+        finalFocusRef={ref}
+        size="md"
+      >
+        <ModalBackdrop />
+        <ModalContent>
+          <ModalHeader>
+            <Heading size="md" className="text-typography-950">
+              Usuń lekarstwo
+            </Heading>
+            <ModalCloseButton>
+              <Icon
+                as={CloseIcon}
+                size="md"
+                className="stroke-background-400 group-[:hover]/modal-close-button:stroke-background-700 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900"
+              />
+            </ModalCloseButton>
+          </ModalHeader>
+          <ModalBody>
+            <Text size="sm" className="text-typography-500">
+              Czy na pewno chcesz usunąć to lekarstwo? Ta operacja jest
+              nieodwracalna i wszystkie dane związane z tym lekiem zostaną
+              utracone.
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              variant="outline"
+              action="secondary"
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Anuluj</ButtonText>
+            </Button>
+            <Button
+              onPress={() => {
+                setShowModal(false);
+              }}
+            >
+              <ButtonText>Potwierdź</ButtonText>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
--- src/screens/medication/MedicationStockScreen.tsx
+++ src/screens/medication/MedicationStockScreen.tsx
@@ -1,22 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, Text, View } from "react-native";
 import MedicationItemStock from "./MedicationItemStock";
 import MessageCard from "@/src/components/shared/MessageCard";
 import EmptyListImage from "@/src/assets/images/empty-list-image.svg";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const MedicationStockScreen = () => {
-  const [medications, setMedications] = useState([
-    { id: 1, name: "Ibuprom" },
-    {
-      id: 2,
-      name: "Ibuprom",
-    },
-  ]);
+  const [medications, setMedications] = useState([]);
+  console.log(medications);
+
+  useEffect(() => {
+    const fetchMedications = async () => {
+      try {
+        const storedData = await AsyncStorage.getItem("medications");
+        if (storedData !== null) {
+          setMedications(JSON.parse(storedData));
+        }
+      } catch (error) {
+        console.error("Błąd podczas odczytywania danych", error);
+      }
+    };
+
+    fetchMedications();
+  }, []);
   return (
     <View>
       <FlatList
         data={medications}
-        renderItem={({ item }) => <MedicationItemStock name={item.name} />}
+        renderItem={({ item }) => <MedicationItemStock medication={item} />}
         ListEmptyComponent={
           <MessageCard
             Image={EmptyListImage}
@@ -24,7 +35,8 @@ const MedicationStockScreen = () => {
             subtitle="Dodaj swój pierwszy lek"
           />
         }
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item: any) => item.id.toString()}
+        ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
       />
     </View>
   );
